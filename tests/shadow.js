22a6ad7c513b6535e584c89f2fa66acfdbe60fc1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-01-25 14:05:47 +0100

    Add tests for declarative Shadow DOM + some code refactoring

--- tests/shadow.js
+++ tests/shadow.js
@@ -3,6 +3,44 @@ import gentleRegisterProperty from "../src/util/gentle-register-property.js";
 
 export default {
 	name: "Shadow DOM",
+
+	beforeEach () {
+		let host = document.getElementById(this.data.hostId);
+		let target = Object.assign(document.createElement("div"), { id: this.arg.property });
+		host.shadowRoot.append(target);
+	},
+
+	async run ({property, meta, initial, value}) {
+		let host = document.getElementById(this.data.hostId);
+		let target = host.shadowRoot.getElementById(this.arg.property);
+
+		if (meta) {
+			gentleRegisterProperty(property, meta);
+		}
+
+		if (initial !== undefined) {
+			target.style.setProperty(property, initial);
+		}
+
+		let records = await new Promise(resolve => {
+			let observer = new StyleObserver(records => {
+				resolve(records);
+			});
+
+			observer.observe(target, property);
+
+			target.style.setProperty(property, value);
+		});
+
+		return records[0].value;
+	},
+
+	afterEach () {
+		let host = document.getElementById(this.data.hostId);
+		let target = host.shadowRoot.getElementById(this.arg.property);
+		target.remove();
+	},
+
 	tests: [
 		{
 			name: "Imperative",
@@ -12,42 +50,11 @@ export default {
 				host.shadowRoot.innerHTML = "";
 				document.body.append(host);
 			},
-			beforeEach () {
-				let host = document.getElementById("imperative-host");
-				let target = Object.assign(document.createElement("div"), { id: this.arg.property });
-				host.shadowRoot.append(target);
-			},
-			async run ({property, meta, initial, value}) {
-				let host = document.getElementById("imperative-host");
-				let target = host.shadowRoot.getElementById(this.arg.property);
-
-				if (meta) {
-					gentleRegisterProperty(property, meta);
-				}
-
-				if (initial) {
-					target.style.setProperty(property, initial);
-				}
-
-				let records = await new Promise(resolve => {
-					let observer = new StyleObserver(records => {
-						resolve(records);
-					});
-
-					observer.observe(target, property);
-
-					target.style.setProperty(property, value);
-				});
-
-				return records[0].value;
-			},
-			afterEach () {
-				let host = document.getElementById("imperative-host");
-				let target = host.shadowRoot.getElementById(this.arg.property);
-				target.remove();
-			},
 			afterAll () {
 				document.getElementById("imperative-host").remove();
+			},
+			data: {
+				hostId: "imperative-host",
 			},
 			tests: [
 				{
@@ -90,5 +97,56 @@ export default {
 				},
 			],
 		},
+		{
+			name: "Declarative",
+			data: {
+				hostId: "declarative-host",
+			},
+			tests: [
+				{
+					name: "Interpolable built-in",
+					arg: {
+						property: "color",
+						value: "red",
+					},
+					expect: "rgb(255, 0, 0)",
+				},
+				{
+					name: "Discrete built-in",
+					arg: {
+						property: "display",
+						initial: "flex",
+						value: "grid",
+					},
+					expect: "grid",
+				},
+				{
+					name: "Unregistered custom property",
+					arg: {
+						property: "--declarative-unregistered",
+						initial: 0,
+						value: 1,
+					},
+					expect: "1",
+				},
+				{
+					name: "Registered custom property",
+					arg: {
+						property: "--declarative-registered",
+						meta: {
+							syntax: "<angle>",
+							initialValue: "45deg",
+						},
+						initial: "0deg",
+						value: "1turn",
+					},
+					expect: "1turn",
+				},
+			],
+		},
+		// {
+		// 	name: "Registered custom element",
+		// 	tests: [],
+		// },
 	],
 };
