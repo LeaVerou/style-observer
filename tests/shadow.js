71c39676991ffe0500c0947bb1665f83e8c0a1e6
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-01-25 11:39:59 +0100

    Address feedback: Correct tests for imperative Shadow DOM

--- tests/shadow.js
+++ tests/shadow.js
@@ -1,91 +1,94 @@
 import StyleObserver from "../src/index.js";
-import { delay } from "../src/util.js";
-
-// Shared state
-let host;
+import gentleRegisterProperty from "../src/util/gentle-register-property.js";
 
 export default {
 	name: "Shadow DOM",
-
-	beforeAll () {
-		host = document.createElement("dummy-element");
-		host.attachShadow({ mode: "open" });
-		host.shadowRoot.innerHTML = "";
-		document.body.append(host);
-	},
-
-	beforeEach () {
-		let dummy = document.createElement("div");
-		host.shadowRoot.append(dummy);
-
-		let observer = new StyleObserver(records => {
-			this.data.record = records[0];
-		});
-
-		let { property, initial } = this.data;
-		if (initial) {
-			dummy.style.setProperty(property, initial);
-		}
-
-		observer.observe(dummy, property);
-
-		this.data.observer = observer;
-		this.data.dummy = dummy;
-	},
-
-	async run (arg) {
-		this.data.dummy.style.setProperty(this.data.property, arg);
-
-		// Wait for the observer to update the record
-		await delay(100);
-
-		return this.data.record.value;
-	},
-
-	afterEach () {
-		this.data.observer.unobserve(this.data.dummy);
-		this.data.dummy.remove();
-	},
-
-	afterAll () {
-		host.remove();
-	},
-
-	getName () {
-		return this.data.property + ": " + (this.data.initial ?? "initial");
-	},
-
 	tests: [
 		{
-			data: {
-				property: "width",
+			name: "Imperative",
+			beforeAll () {
+				let host = Object.assign(document.createElement("dummy-element"), { id: "imperative-host" });
+				host.attachShadow({ mode: "open" });
+				host.shadowRoot.innerHTML = "";
+				document.body.append(host);
 			},
-			arg: "42px",
-			expect: "42px",
-		},
-		{
-			data: {
-				property: "text-align",
-				initial: "center",
+			beforeEach () {
+				let host = document.getElementById("imperative-host");
+				let target = Object.assign(document.createElement("div"), { id: this.arg.property });
+				host.shadowRoot.append(target);
 			},
-			arg: "right",
-			expect: "right",
-		},
-		{
-			data: {
-				property: "opacity",
-				initial: "0.5",
+			async run ({property, meta, initial, value}) {
+				let host = document.getElementById("imperative-host");
+				let target = host.shadowRoot.getElementById(this.arg.property);
+
+				if (meta) {
+					gentleRegisterProperty(property, meta);
+				}
+
+				if (initial) {
+					target.style.setProperty(property, initial);
+				}
+
+				let records = await new Promise(resolve => {
+					let observer = new StyleObserver(records => {
+						resolve(records);
+					});
+
+					observer.observe(target, property);
+
+					target.style.setProperty(property, value);
+				});
+
+				return records[0].value;
 			},
-			arg: "0",
-			expect: "0",
-		},
-		{
-			data: {
-				property: "--color",
-				initial: "transparent",
+			afterEach () {
+				let host = document.getElementById("imperative-host");
+				let target = host.shadowRoot.getElementById(this.arg.property);
+				target.remove();
 			},
-			arg: "rebeccapurple",
-			expect: "rebeccapurple",
+			afterAll () {
+				document.getElementById("imperative-host").remove();
+			},
+			tests: [
+				{
+					name: "Interpolable built-in",
+					arg: {
+						property: "opacity",
+						value: "0.5",
+					},
+					expect: "0.5",
+				},
+				{
+					name: "Discrete built-in",
+					arg: {
+						property: "display",
+						value: "flex",
+					},
+					expect: "flex",
+				},
+				{
+					name: "Unregistered custom property",
+					arg: {
+						property: "--unregistered",
+						initial: "bar",
+						value: "foo",
+					},
+					expect: "foo",
+				},
+				{
+					name: "Registered custom property",
+					arg: {
+						property: "--registered",
+						meta: {
+							syntax: "<color>",
+							initialValue: "transparent",
+						},
+						initial: "red",
+						value: "green",
+					},
+					expect: "green",
+				},
+			],
 		},
 	],
 };
