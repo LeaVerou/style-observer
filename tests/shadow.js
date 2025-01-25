c0036e95a56a0543535d3385eac065108007792a
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-01-25 18:24:52 +0100

    [tests] Shadow DOM (#9)

---
+++ tests/shadow.js
@@ -0,0 +1,96 @@
+import StyleObserver from "../src/index.js";
+import gentleRegisterProperty from "../src/util/gentle-register-property.js";
+import tests from "./tests.js";
+
+export default {
+	name: "Shadow DOM",
+
+	beforeEach () {
+		let host = document.getElementById(this.data.hostId);
+		let target = Object.assign(document.createElement("div"), { id: this.arg.property });
+		host.shadowRoot.append(target);
+	},
+
+	async run ({property, meta, initial, value}) {
+		let host = document.getElementById(this.data.hostId);
+		let target = host.shadowRoot.getElementById(this.arg.property);
+
+		if (property.startsWith("--")) {
+			// Make custom properties unique across all tests
+			property += "-" + this.data.hostId;
+		}
+
+		if (meta) {
+			gentleRegisterProperty(property, meta);
+		}
+
+		if (initial !== undefined) {
+			target.style.setProperty(property, initial);
+		}
+
+		let records = await new Promise(resolve => {
+			let observer = new StyleObserver(records => {
+				resolve(records);
+			});
+
+			observer.observe(target, property);
+
+			target.style.setProperty(property, value);
+		});
+
+		return records[0].value;
+	},
+
+	afterEach () {
+		let host = document.getElementById(this.data.hostId);
+		let target = host.shadowRoot.getElementById(this.arg.property);
+		target.remove();
+	},
+
+	tests: [
+		{
+			name: "Imperative",
+			beforeAll () {
+				let host = Object.assign(document.createElement("div"), { id: "imperative-host" });
+				host.attachShadow({ mode: "open" });
+				host.shadowRoot.innerHTML = "";
+				document.body.append(host);
+			},
+			afterAll () {
+				document.getElementById("imperative-host").remove();
+			},
+			data: {
+				hostId: "imperative-host",
+			},
+			tests,
+		},
+		{
+			name: "Declarative",
+			data: {
+				hostId: "declarative-host",
+			},
+			tests,
+		},
+		{
+			name: "Registered custom element",
+			async beforeAll () {
+				class RegisteredComponent extends HTMLElement {
+					constructor () {
+						super();
+						this.attachShadow({ mode: "open" });
+					}
+
+					connectedCallback () {
+						this.setAttribute("id", "registered-host");
+					}
+				}
+
+				customElements.define("registered-component", RegisteredComponent);
+			},
+			data: {
+				hostId: "registered-host",
+			},
+			tests,
+		},
+	],
+};
---
+++ tests/tests.js
@@ -0,0 +1,40 @@
+export default [
+	{
+		name: "Interpolable built-in",
+		arg: {
+			property: "opacity",
+			value: "0.5",
+		},
+		expect: "0.5",
+	},
+	{
+		name: "Discrete built-in",
+		arg: {
+			property: "display",
+			value: "flex",
+		},
+		expect: "flex",
+	},
+	{
+		name: "Unregistered custom property",
+		arg: {
+			property: "--unregistered",
+			initial: "bar",
+			value: "foo",
+		},
+		expect: "foo",
+	},
+	{
+		name: "Registered custom property",
+		arg: {
+			property: "--registered",
+			meta: {
+				syntax: "<angle>",
+				initialValue: "0deg",
+			},
+			initial: "45deg",
+			value: "1turn",
+		},
+		expect: "360deg",
+	},
+];
