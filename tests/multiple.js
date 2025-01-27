db9e3594fef85f4738dd148c29dac8528ea3a1ef
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-01-27 16:46:27 +0100

    [tests] Update tests for multiple observers on one target (#26)

--- tests/multiple.js
+++ tests/multiple.js
@@ -1,84 +1,155 @@
 import StyleObserver from "../src/index.js";
-import { delay } from "../src/util.js";
-
-// Shared state
-let dummy;
-let observers = {
-	1: null,
-	2: null,
-};
-let records = {
-	1: [],
-	2: [],
-};
+import gentleRegisterProperty from "../src/util/gentle-register-property.js";
 
 export default {
-	name: "Multiple observers",
+	name: "Multiple observers on the same target",
 
 	beforeAll () {
-		dummy = document.createElement("div");
-		document.body.append(dummy);
-
-		observers[1] = new StyleObserver(recs => {
-			records[1] = recs;
-		}, { target: dummy });
-
-		observers[2] = new StyleObserver(recs => {
-			records[2] = recs;
-		}, { target: dummy });
+		gentleRegisterProperty("--prop-registered", { syntax: "<percentage>", initialValue: "0%" });
 	},
 
 	beforeEach () {
-		let { property, initial } = this.data;
+		this.dummy = document.createElement("div");
+		document.body.append(this.dummy);
 
-		if (initial) {
-			dummy.style.setProperty(property, initial);
-		}
-
-		observers[this.args[0]].observe(property);
+		this.dummy.style.setProperty("--prop-not-registered", "foo");
 	},
 
-	async run (observer, value) {
-		let property = this.data.property;
+	run ({ props1, props2, property, value }) {
+		let observer1, observer2, ret = [];
 
-		dummy.style.setProperty(property, value);
+		let wait = props1.some(prop => props2.includes(prop));
 
-		// Wait for the observer to update the record
-		await delay(100);
+		let callback = (records, resolve) => {
+			ret.push(...records);
+			if (wait) {
+				if (ret.length === 2) {
+					// We got records from both observers
+					resolve(ret);
+				}
+			}
+			else {
+				resolve(ret);
+			}
+		};
 
-		// Find the record for the property
-		let record = records[observer].find(record => record.property === property);
+		let promise1 = new Promise((resolve, reject) => {
+			observer1 = new StyleObserver(records => callback(records, resolve), { target: this.dummy, properties: props1 });
 
-		return record.value;
+			setTimeout(reject, 500, "First observer timed out");
+		});
+
+		let promise2 = new Promise((resolve, reject) => {
+			observer2 = new StyleObserver(records => callback(records, resolve), { target: this.dummy, properties: props2 });
+
+			setTimeout(reject, 500, "Second observer timed out");
+		});
+
+		this.dummy.style.setProperty(property, value);
+
+		return Promise.race([promise1, promise2])
+		.then(records => records.map(record => record.value))
+		.catch(reason => reason)
+		.finally(() => {
+			observer1.unobserve();
+			observer2.unobserve();
+		});
 	},
 
 	afterEach () {
-		observers[this.args[0]].unobserve(dummy, this.data.property);
-	},
-
-	afterAll () {
-		dummy.remove();
-	},
-
-	getName () {
-		return this.data.property + ": " + (this.data.initial ?? "initial") + " (observer " + this.args[0] + ")";
+		this.dummy.remove();
 	},
 
 	tests: [
 		{
-			data: {
-				property: "width",
-			},
-			args: [1, "42px"],
-			expect: "42px",
+			name: "Without properties overlap",
+			tests: [
+				{
+					name: "Interpolable built-in (from first observer)",
+					arg: {
+						props1: ["opacity", "text-align"],
+						props2: ["padding", "background-color"],
+						property: "opacity",
+						value: "0.5",
+					},
+					expect: ["0.5"],
+				},
+				{
+					name: "Discrete built-in (from second observer)",
+					arg: {
+						props1: ["padding", "background-color"],
+						props2: ["opacity", "text-align"],
+						property: "text-align",
+						value: "center",
+					},
+					expect: ["center"],
+				},
+				{
+					name: "Registered custom property (from first observer)",
+					arg: {
+						props1: ["--prop-registered", "text-align"],
+						props2: ["padding", "background-color"],
+						property: "--prop-registered",
+						value: "50%",
+					},
+					expect: ["50%"],
+				},
+				{
+					name: "Not registered custom property (from second observer)",
+					arg: {
+						props1: ["padding", "background-color"],
+						props2: ["--prop-not-registered", "text-align"],
+						property: "--prop-not-registered",
+						value: "bar",
+					},
+					expect: ["bar"],
+				}
+			],
 		},
 		{
-			data: {
-				property: "--color",
-				initial: "transparent",
-			},
-			args: [2, "rebeccapurple"],
-			expect: "rebeccapurple",
+			name: "With properties overlap",
+			tests: [
+				{
+					name: "Interpolable built-in",
+					arg: {
+						props1: ["opacity", "text-align"],
+						props2: ["opacity", "color"],
+						property: "opacity",
+						value: "0.5",
+					},
+					expect: ["0.5", "0.5"],
+				},
+				{
+					name: "Discrete built-in",
+					arg: {
+						props1: ["text-align", "opacity"],
+						props2: ["padding", "text-align"],
+						property: "text-align",
+						value: "right",
+					},
+					expect: ["right", "right"],
+				},
+				{
+					name: "Registered custom property",
+					arg: {
+						props1: ["--prop-registered", "text-align"],
+						props2: ["padding", "--prop-registered"],
+						property: "--prop-registered",
+						value: "50%",
+					},
+					expect: ["50%", "50%"],
+				},
+				{
+					name: "Not registered custom property",
+					arg: {
+						props1: ["padding", "--prop-not-registered"],
+						props2: ["text-align", "--prop-not-registered"],
+						property: "--prop-not-registered",
+						value: "baz",
+					},
+					expect: ["baz", "baz"],
+				}
+			],
 		},
 	],
 };
