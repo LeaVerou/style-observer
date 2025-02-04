02f2a5caf2fc533ad2d2da92e298f377e50759e8
Lea Verou <lea@verou.me>
2025-02-04 08:56:50 -0500

    `parseTime()`

--- src/util.js
+++ src/util.js
@@ -17,3 +17,28 @@ export function delay(ms) {
 
 	return new Promise(resolve => requestAnimationFrame(resolve));
 }
+
+/**
+ * Parse a CSS <time> value
+ * @param {string | number} cssTime - A CSS <time> value
+ * @return {number} The time in milliseconds
+ */
+export function parseTime (cssTime) {
+	if (typeof cssTime === "number") {
+		return cssTime;
+	}
+
+	let match = cssTime.match(/^([+-]?(?:\d+|\d*\.\d+))\s*(ms|s)?$/);
+	if (!match) {
+		throw new Error(`Invalid time value: ${ cssTime }`);
+	}
+
+	let [, value, unit] = match;
+	value = parseFloat(value);
+
+	if (unit === "s") {
+		value *= 1000;
+	}
+
+	return value;
+}
