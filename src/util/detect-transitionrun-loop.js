880f880fb72b4befd37168f097f13efcb819a50a
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-02-05 16:42:15 +0100

    Address feedback: mention the `transitionstart` event

--- src/util/detect-transitionrun-loop.js
+++ src/util/detect-transitionrun-loop.js
@@ -1,5 +1,5 @@
 // In Safari < 18.2, transitioning custom properties of syntax `*` or `<string>`
-// causes an infinite loop of `transitionrun` events.
+// causes an infinite loop of `transitionrun` or `transitionstart` events.
 // We use this test to detect the bug.
 let dummy = document.createElement("div");
 document.body.appendChild(dummy);
