a7c5722d9cd8dc94e9cd6d23fbab6820eed8dff7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-02-07 16:10:07 +0100

    [util] Improve property registration (#47)
    * Try registering property in case of a syntax error (not supported syntax, not parsable initial value, etc.)
    
    * Add support for sensible defaults
    
    ---------
    
    Co-authored-by: Lea Verou <lea@verou.me>

--- src/util/gentle-register-property.js
+++ src/util/gentle-register-property.js
@@ -1,3 +1,21 @@
+const INITIAL_VALUES = {
+	"<angle>": "0deg",
+	"<color>": "transparent",
+	"<custom-ident>": "none",
+	"<image>": "linear-gradient(transparent 0% 100%)",
+	"<integer>": "0",
+	"<length>": "0px",
+	"<length-percentage>": "0px",
+	"<number>": "0",
+	"<percentage>": "0%",
+	"<resolution>": "1dppx",
+	"<string>": "''",
+	"<time>": "0s",
+	"<transform-function>": "scale(1)",
+	"<transform-list>": "scale(1)",
+	"<url>": "url('')",
+};
+
 /**
  * Register a CSS custom property if it’s not already registered
  * @param {string} property - Property name
@@ -11,24 +29,52 @@ export default function gentleRegisterProperty (property, meta = {}) {
 		return;
 	}
 
+	let definition = {
+		name: property,
+		syntax: meta.syntax || "*",
+		inherits: meta.inherits ?? true,
+	};
+
+	if (meta.initialValue !== undefined) {
+		definition.initialValue = meta.initialValue;
+	}
+	else if (definition.syntax !== "*" && definition.syntax in INITIAL_VALUES) {
+		definition.initialValue = INITIAL_VALUES[definition.syntax];
+	}
+
 	try {
-		let definition = {
-			name: property,
-			syntax: meta.syntax || "*",
-			inherits: meta.inherits ?? true,
-		};
-
-		if (meta.initialValue !== undefined) {
-			definition.initialValue = meta.initialValue;
-		}
-
 		CSS.registerProperty(definition);
 	}
 	catch (e) {
-		// Property is already registered, which is fine
-		if (!(e instanceof DOMException && e.name === "InvalidModificationError")) {
+		let error = e;
+		let rethrow = true;
+
+		if (e instanceof DOMException) {
+			if (e.name === "InvalidModificationError") {
+				// Property is already registered, which is fine
+				rethrow = false;
+			}
+			else if (e.name === "SyntaxError") {
+				// In Safari < 18.2 (where we face the infinite loop bug),
+				// there is no way to provide an initial value for a custom property with a syntax of "<string>".
+				// There will always be an error: “The given initial value does not parse for the given syntax.”
+				// So we try again with universal syntax.
+				// We do the same for any other syntax that is not supported.
+				definition.syntax = "*";
+
+				try {
+					CSS.registerProperty(definition);
+					rethrow = false;
+				}
+				catch (e) {
+					error = e;
+				}
+			}
+		}
+
+		if (rethrow) {
 			// Re-throw any other errors
-			throw new Error(`Failed to register custom property ${ property }: ${ e.message }`, { cause: e });
+			throw new Error(`Failed to register custom property ${ property }: ${ error.message }`, { cause: error });
 		}
 	}
 }
