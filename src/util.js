65ff74ecf4dd543bb7e71528baa0f564629570b3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-02-07 11:18:44 +0100

    Address feedback: simplify code

--- src/util.js
+++ src/util.js
@@ -91,20 +91,6 @@ export function getTimesFor (property, transitions) {
 	return { duration, delay };
 }
 
-export const defaultPairs = {
-	nest: {
-		"(": ")",
-		"[": "]",
-		"{": "}",
-	},
-	ignore: {
-		'"': '"',
-		// "'": "'",
-		// "`": "`",
-	},
-
-};
-
 /**
  * Escape special characters in a string for use in a regular expression.
  * @param {string} string
@@ -115,37 +101,30 @@ export function regexEscape (string) {
 }
 
 /**
- * Split a value by a separator, respecting pairs (parens, strings, etc.) but failing back gracefully for malformed input.
- * @param {string} value
- * @param {object} options
- * @param {string} [options.separator] - The separator to split on. Defaults to `,`.
- * @param {object} [options.pairs] - The pairs to respect. Defaults to {@link defaultPairs}.
+ * Split a value by a separator, respecting parentheses.
+ * @param {string} value - The value to split.
+ * @param {string} [separator] - The separator to split on. Defaults to `,`.
  * @returns {string[]} The split values.
  */
-export function split (value, { separator = ",", pairs = defaultPairs } = {}) {
+export function split (value, separator = ",") {
 	value = value.trim();
 
-	// Make whitespace optional and flexible, unless the separator consists entirely of whitespace
+	// Make whitespace optional and flexible
 	separator = separator.trim();
-	let isSeparatorWhitespace = !separator;
-	let separatorRegex = isSeparatorWhitespace ? /\s+/g : RegExp(regexEscape(separator).replace(/^\s*|\s*$/g, "\\s*"), "g");
+	let separatorRegex = RegExp(regexEscape(separator).replace(/^\s*|\s*$/g, "\\s*"), "g");
+	let parensRegex = /\(|\)/g;
 
-	let pairStrings = new Set([...Object.keys(pairs.nest), ...Object.values(pairs.nest), ...Object.keys(pairs.ignore), ...Object.values(pairs.ignore)]);
-	let pairRegex = RegExp([...pairStrings].map(regexEscape).join("|"), "g");
-
-	if (!pairRegex.test(value)) {
-		// value contains no pairs, just split
+	if (!parensRegex.test(value)) {
+		// value contains no parentheses, just split
 		return value.trim().split(separatorRegex);
 	}
 
-	let invertedNestPairs = Object.fromEntries(Object.entries(pairs.nest).map(([start, end]) => [end, start]));
-	let splitter = RegExp([separatorRegex.source, pairRegex.source].join("|"), "g");
+	let splitter = RegExp([separatorRegex.source, parensRegex.source].join("|"), "g");
 	let stack = [];
 	let items = [];
 	let item = "";
 	let matches = [...value.matchAll(splitter)];
 	let lastIndex = 0;
-	let ignoreUntil;
 
 	for (let i = 0; i < matches.length; i++) {
 		let match = matches[i];
@@ -153,50 +132,19 @@ export function split (value, { separator = ",", pairs = defaultPairs } = {}) {
 		let matched = match[0];
 		let part = value.slice(lastIndex, index);
 
-		if (ignoreUntil) {
-			if (ignoreUntil === matched) {
-				// TODO escape?
-				ignoreUntil = null;
-			}
-		}
-		else if (matched.trim() === separator) {
+		if (matched.trim() === separator) {
 			if (stack.length === 0) {
-				// No open pairs, add to items
+				// No open parentheses, add to items
 				items.push(part.trim());
 				lastIndex = index + matched.length;
 			}
 		}
-		else if (pairs.ignore[matched]) {
-			// Opening ignored (verbatim) pair
-			let closingPair = pairs.ignore[matched];
-
-			// Do we have its closing pair in the string?
-			if (matches.slice(i + 1).find(m => m[0] === closingPair)) {
-				ignoreUntil = closingPair;
-			}
+		else if (matched === "(") {
+			stack.push("(");
 		}
-		else if (pairs.nest[matched]) {
-			// Opening nested pair
-			let closingPair = pairs.nest[matched];
-
-			// Do we have its closing pair in the string?
-			if (matches.slice(i + 1).find(m => m[0] === closingPair)) {
-				stack.push(matched);
-			}
+		else if (matched === ")") {
+			stack.pop();
 		}
-		else if (invertedNestPairs[matched]) {
-			// Closing nested pair
-			// Why not just check and pop? We want malformed (interleaved) pairs to work too
-			let startIndex = stack.findLastIndex(start => start === invertedNestPairs[matched]);
-
-			if (startIndex > -1) {
-				stack.splice(startIndex, 1);
-			}
-		}
-	}
-
-	if (stack.length > 0 || ignoreUntil) {
-		// Malformed pairs, just split
 	}
 
 	if (lastIndex < value.length || item.length > 0) {
