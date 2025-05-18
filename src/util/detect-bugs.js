76189a2c4e90b34e98eed20a123ec80c6d987a6c
Benoît Rouleau <benoit.rouleau@icloud.com>
2025-05-17 20:48:57 -0400

    Fix bug + make it closer to previous syntax

--- src/util/detect-bugs.js
+++ src/util/detect-bugs.js
@@ -5,26 +5,28 @@ import detectUnregisteredTransitionBug from "./detect-bugs/unregistered-transiti
 let transitionRunLoopPromise = null;
 let unregisteredTransitionPromise = null;
 
-/**
- * Check if the browser is affected by the Safari transitionrun loop bug
- * @returns {Promise<boolean>}
- */
-export function hasTransitionRunLoopBug() {
-	if (!transitionRunLoopPromise) {
-		transitionRunLoopPromise = detectTransitionRunLoopBug();
-	}
-	return transitionRunLoopPromise;
-}
+const bugs = {
+	/**
+	 * Check if the browser is affected by the Safari transitionrun loop bug
+	 * @returns {Promise<boolean>}
+	 */
+	async transitionRunLoop() {
+		if (!transitionRunLoopPromise) {
+			transitionRunLoopPromise = detectTransitionRunLoopBug();
+		}
+		return transitionRunLoopPromise;
+	},
 
-/**
- * Check if the browser is affected by the unregistered transition bug
- * @returns {Promise<boolean>}
- */
-export function hasUnregisteredTransitionBug() {
-	if (!unregisteredTransitionPromise) {
-		unregisteredTransitionPromise = detectUnregisteredTransitionBug();
+	/**
+	 * Check if the browser is affected by the unregistered transition bug
+	 * @returns {Promise<boolean>}
+	 */
+	async unregisteredTransition() {
+		if (!unregisteredTransitionPromise) {
+			unregisteredTransitionPromise = detectUnregisteredTransitionBug();
+		}
+		return unregisteredTransitionPromise;
 	}
-	return unregisteredTransitionPromise;
-}
+};
 
-export { detectTransitionRunLoopBug, detectUnregisteredTransitionBug };
+export default bugs;
