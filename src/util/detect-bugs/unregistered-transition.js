b04c8c05c14ba4e46ae285b1dfcc7d82b1025f43
Benoît Rouleau <benoit.rouleau@icloud.com>
2025-05-18 12:04:32 -0400

    Make `observe` synchronous again

--- src/util/detect-bugs/unregistered-transition.js
+++ src/util/detect-bugs/unregistered-transition.js
@@ -1,21 +1,29 @@
 // Detect https://issues.chromium.org/issues/360159391
 
+let result = null;
+
 /**
  * Detect if the browser is affected by the unregistered transition bug.
  * @see https://issues.chromium.org/issues/360159391
  * @returns {Promise<boolean>}
  */
-export default function detectUnregisteredTransitionBug() {
+export default function detectUnregisteredTransitionBug () {
+	if (result !== null) {
+		return result;
+	}
+
 	let dummy = document.createElement("div");
 	document.body.appendChild(dummy);
 	let property = "--foo-" + Date.now();
 	dummy.style.cssText = `${property}: 1; transition: ${property} 1ms step-start allow-discrete`;
 
-	return new Promise(resolve => {
+	result = new Promise(resolve => {
 		requestAnimationFrame(() => {
 			setTimeout(_ => resolve(true), 30);
 			dummy.addEventListener("transitionstart", _ => resolve(false));
 			dummy.style.setProperty(property, "2");
 		});
 	}).finally(() => dummy.remove());
+
+	return result;
 }
